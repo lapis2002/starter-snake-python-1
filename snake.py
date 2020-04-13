323b8a1ffae28210b112fe7e31c93564fcecd723
lapis2002 <irene.duong799@gmail.com>
2020-04-13 12:43:33 -0700

    fix error tail

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ class Snake(Point):
         self.health = snake["health"]
         self.body, self.len = self.set_body(snake, value)
         self.head = self.body[0]
-        self.tail - self.body[-1]
+        self.tail = self.body[-1]
         self.next_move = ""
 
     def set_body(self, snake, value):
